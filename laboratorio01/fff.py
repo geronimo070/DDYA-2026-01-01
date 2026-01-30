63bfdeb4de63294e3eefdcffb94c4b476b258378
unknown <geronimo.fuentes-p@labinfo.is.escuelaing.edu.co>
2026-01-30 08:25:51 -0500

    primer commit del laboratorio

---
+++ laboratorio01/fff.py
@@ -0,0 +1,15 @@
+from sys import stdin
+
+def main():
+    t= int(stdin.readline())
+    for i in range (t):
+        a= int(stdin.readline())
+        b= int (b)
+        a= int (a)
+        if a > b:
+            print(">")
+        elif a < b:
+            print ("<"
+        else a == b:
+            print("=")
+main()
