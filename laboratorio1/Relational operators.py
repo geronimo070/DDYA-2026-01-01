0ad33d3f22f5c77b645766addbdb06638a21f1d8
unknown <geronimo.fuentes-p@labinfo.is.escuelaing.edu.co>
2026-02-06 08:16:40 -0500

    entrega del problema 11172 ya que no me aparecia

---
+++ laboratorio1/Relational operators.py
@@ -0,0 +1,18 @@
+from sys import stdin
+
+def main():
+    t= int(stdin.readline())    #ejercicio 11172 lab 01
+    resultados = []
+    for i in range (t):
+        a, b= stdin.readline().split()
+        a= int (a)
+        b= int(b)
+        if a > b:
+            resultados.append(">")
+        elif a < b:
+            resultados.append("<")
+        else:
+            resultados.append("=")
+    for r in resultados:
+        print(r)
+main()
