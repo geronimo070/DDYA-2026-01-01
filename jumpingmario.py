ca50d1b70d5c22a5db9915d200649a640354b119
unknown <geronimo.fuentes-p@labinfo.is.escuelaing.edu.co>
2026-02-06 09:27:21 -0500

    avance de jumping mario

---
+++ jumpingmario.py
@@ -0,0 +1,9 @@
+from sys import stdin
+def contador_saltos():
+    T= int(stdin.readline().split())
+    salto_alto = 0
+    salto_bajo= 0
+    for i in range T:
+        N = int(stdin.split())
+def main():
+main()
